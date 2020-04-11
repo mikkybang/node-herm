a2c412905f925f01738a4c2a358444af2add7eed
Eunovo <eunovo9@gmail.com>
2020-04-11 06:28:10 +0100

    Use express session

--- packages/node-herm-web/index.js
+++ packages/node-herm-web/index.js
@@ -1,11 +1,21 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const session = require('express-session');
+const app = express();
+
+app.use(session({ 
+    secret: 'no_seceret',
+    resave: true,
+    saveUninitialized: true,
+}));
 
 app.set('view engine', 'ejs');
 
-// index page 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('pages/index');
+});
+
+app.get('/add/model', function (req, res) {
+    res.render('pages/add-model', { data: req.session.data });
 });
 
 app.listen(8080);
