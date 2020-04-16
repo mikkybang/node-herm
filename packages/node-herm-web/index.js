83c1962e40353948ba0657ba6015f9f13e47c4cd
Eunovo <eunovo9@gmail.com>
2020-04-16 20:09:17 +0100

    Implement model addition and view
    Store models in req,session

--- packages/node-herm-web/index.js
+++ packages/node-herm-web/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const parser = require('body-parser');
 const session = require('express-session');
 const app = express();
 
+app.use(parser.urlencoded({ extended: false }));
+app.use(parser.json());
 app.use(session({ 
     secret: 'no_seceret',
     resave: true,
@@ -14,8 +17,53 @@ app.get('/', function (req, res) {
     res.render('pages/index');
 });
 
-app.get('/add/model', function (req, res) {
-    res.render('pages/add-model', { data: req.session.data });
+app.get('/model/', function (req, res) {
+    res.render('pages/model/index', {
+        data: req.session.data
+    });
+});
+
+app.get('/model/add', function (req, res) {
+    res.render('pages/model/add', { 
+        data: req.session.data,
+        model: {
+            name: '', properties: [
+                { 
+                    name: '', type: ''
+                }
+            ],
+        }
+    });
+});
+
+app.post('/model/add', function (req, res) {
+    let properties= [];
+    const { propertyNames, propertyTypes } = req.body;
+    
+    if (Array.isArray(propertyNames)) {
+        propertyNames.forEach((e, i) => {
+            properties.push({
+                name: e, type: propertyTypes[i]
+            });
+        });
+    } else {
+        properties.push({ name: propertyNames, type: propertyTypes });
+    }
+
+    const data = req.session.data || { models: [] };
+    req.session.data = { 
+        ...data, 
+        models: [
+            ...data.models, 
+            {
+                name: req.body.modelName,
+                properties
+            }
+        ] 
+    }
+    req.session.save((err) => console.log(err));
+
+    res.redirect('/model/');
 });
 
 app.listen(8080);
